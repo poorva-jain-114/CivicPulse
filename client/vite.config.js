94d82d8b2639a5e184f9bbf03c52123d51acaa50
Poorva Jain <jainpd05@gmail.com>
2026-06-26 18:29:54 +0530

    chore: add dev comment to vite config to trigger new deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// Configured with port 3000 for local development
 export default defineConfig({
   plugins: [react()],
   server: {
